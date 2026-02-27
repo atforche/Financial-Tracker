d8d6c0068a09481aac9339f6ee1b328d15c5d56b
atforche <atforche@gmail.com>
2026-02-27 22:00:47 +0000

    Implement hinting and autocomplete on the combo box entry frame

--- frontend/src/framework/dialog/ComboBoxEntryField.tsx
+++ frontend/src/framework/dialog/ComboBoxEntryField.tsx
@@ -1,7 +1,13 @@
-import { Autocomplete, CircularProgress, TextField } from "@mui/material";
+import {
+  Autocomplete,
+  Box,
+  CircularProgress,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { type JSX, useRef, useState } from "react";
 import type ApiErrorHandler from "@data/ApiErrorHandler";
 import ErrorHelperText from "@framework/dialog/ErrorHelperText";
-import type { JSX } from "react";
 
 /**
  * Interface representing a Combo Box option.
@@ -40,40 +46,91 @@ const ComboBoxEntryField = function <T>({
   errorHandler = null,
   errorKey = null,
 }: ComboBoxEntryFieldProps<T>): JSX.Element {
+  const hint = useRef("");
+  const [inputValue, setInputValue] = useState("");
   return (
     <Autocomplete
       className="combo-box-entry-field"
       disabled={setValue === null}
       disableClearable
       options={options}
+      inputValue={inputValue}
       value={value}
       renderInput={(params) => (
-        // @ts-expect-error: The params type from MUI is not correct, even though it comes from their examples and works at runtime.
-        <TextField
-          {...params}
-          label={label}
-          error={(errorHandler?.handleError(errorKey) ?? null) !== null}
-          helperText={
-            <ErrorHelperText errorHandler={errorHandler} errorKey={errorKey} />
-          }
-          slotProps={{
-            input: {
-              ...params.InputProps,
-              endAdornment: (
-                <>
-                  {loading ? (
-                    <CircularProgress color="inherit" size={20} />
-                  ) : null}
-                  {params.InputProps.endAdornment}
-                </>
-              ),
-            },
-          }}
-        />
+        <Box sx={{ position: "relative" }}>
+          <Typography
+            sx={{
+              position: "absolute",
+              opacity: 0.5,
+              left: 14,
+              top: 16.5,
+              overflow: "hidden",
+              whiteSpace: "nowrap",
+              width: "calc(100% - 75px)",
+            }}
+          >
+            {hint.current}
+          </Typography>
+          {/* @ts-expect-error: The params type from MUI is not correct, even though it comes from their examples and works at runtime. */}
+          <TextField
+            {...params}
+            label={label}
+            error={(errorHandler?.handleError(errorKey) ?? null) !== null}
+            helperText={
+              <ErrorHelperText
+                errorHandler={errorHandler}
+                errorKey={errorKey}
+              />
+            }
+            slotProps={{
+              input: {
+                ...params.InputProps,
+                endAdornment: (
+                  <>
+                    {loading ? (
+                      <CircularProgress color="inherit" size={20} />
+                    ) : null}
+                    {params.InputProps.endAdornment}
+                  </>
+                ),
+              },
+            }}
+            onChange={(event) => {
+              const newValue = event.target.value;
+              setInputValue(newValue);
+              const matchingOption = options.find((option) =>
+                option.label.startsWith(newValue),
+              );
+
+              if (newValue && matchingOption) {
+                hint.current = matchingOption.label;
+              } else {
+                hint.current = "";
+              }
+            }}
+          />
+        </Box>
       )}
       loading={loading}
       onChange={(_, newValue) => {
         setValue?.(newValue);
+      }}
+      onKeyDown={(event) => {
+        if (event.key === "Tab") {
+          if (hint.current) {
+            const matchingOption = options.find((option) =>
+              option.label.startsWith(hint.current),
+            );
+            if (matchingOption) {
+              setInputValue(matchingOption.label);
+              setValue?.(matchingOption);
+            }
+          }
+        }
+      }}
+      onClose={() => {
+        hint.current = "";
+        setInputValue(value.label);
       }}
     />
   );
